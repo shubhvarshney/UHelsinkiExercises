f7f84ca94c9abc5efbf9d40f76524e2ff38354f8
shubhvarshney <123425267+shubhvarshney@users.noreply.github.com>
2025-05-28 02:52:22 +0000

    Finished exercises 3.15-3.18

--- Part3/phonebook/index.js
+++ Part3/phonebook/index.js
@@ -4,8 +4,10 @@ const morgan = require('morgan')
 const Person = require('./models/person')
 const app = express()
 
-app.use(express.json())
 app.use(express.static('dist'))
+app.use(express.json())
+app.use(requestLogger)
+
 app.use(morgan(function (tokens, req, res) {
   morgan.token('data', function (req, res) { return JSON.stringify(req.body) })
   if (tokens.method(req, res) == "POST") {
@@ -28,10 +30,6 @@ app.use(morgan(function (tokens, req, res) {
   ].join(' ')
 }))
 
-const generateId = () => {
-    return String(Math.floor(Math.random() * 100000))
-}
-
 app.get('/api/persons', (request, response) => {
     Person.find({}).then(persons => {
         response.json(persons)
@@ -49,17 +47,25 @@ app.get('/info', (request, response) => {
     })
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
     const id = request.params.id
     Person.findById(id).then(person => {
-        response.json(person)
+        if (person) {
+            response.json(person)
+        } else {
+            response.status(404).end()
+        }
     })
+    .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
     const id = request.params.id
-    persons = persons.filter(person => person.id !== id)
-    response.status(204).end()
+    Person.findByIdAndDelete(id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
@@ -71,14 +77,6 @@ app.post('/api/persons', (request, response) => {
         })
     }
 
-    /*
-    if (body.name in persons.map(p => p.name)) {
-        return response.status(400).json({
-            error: 'name must be unique'
-        })
-    }
-    */
-
     const person = new Person({
         name: body.name,
         number: body.number,
@@ -89,7 +87,47 @@ app.post('/api/persons', (request, response) => {
     })
 })
 
+app.put('/api/persons/:id', (request, response, next) => {
+    const { name, number } = request.body
+
+    Person.findById(request.params.id)
+        .then(person => {
+            if (!person) {
+                return response.status(404).end()
+            }
+
+            person.name = name
+            person.number = number
+
+            return person.save().then(updatedPerson => {
+                response.json(updatedPerson)
+            })
+        })
+        .catch(error => next(error))
+
+        
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+app.use(errorHandler)
+
 const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
+
